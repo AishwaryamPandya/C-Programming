869dd85aba31fb517f40b4d0d162fc9746af46f8
ash3616 <aishwaryam.pandya@hotmail.com>
2017-09-06 20:39:50 -0400

    First C Code

---
+++ First_Program.c
@@ -0,0 +1,33 @@
+/*
+* Name: First Program
+* Description: This is a sample code to get users started with the basic of C.
+* Date: September 6th 2017
+* Author: Aishwaryam Pandya
+*/
+//===================================================================//
+// This is a Single Line comment                                     //
+//===================================================================//
+/* This 
+   is a
+   multi-line
+   comment   */
+//===================================================================//
+//                             INCLUDE                               //
+//===================================================================//
+
+// The include is used to add header files to the code. //
+#include<stdio.h>
+
+//===================================================================//
+//                              MAIN                                 //
+//===================================================================//
+
+int main() // This is the main body of your code 
+
+{ // program begins in braces"curly braces".
+       // printf() displays the string inside quotation. 
+    printf("Hello, This is My First Code");
+    return 0; // Return is used to get the value of the varibale or other data coming back from subroutine
+} // program ends in braces "curly braces"
+
+//===================================================================//
